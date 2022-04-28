ad7c9330cbf8f1a94dcee8285ca42fbf62d180dd
bits-wa <bitswa@pm.me>
2022-04-27 22:32:26 -0300

    new project with API

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,14 @@
+const id = document.querySelector('.c-advice__id');
+const advice = document.querySelector('.c-quote');
+
+const fetchAdvice = async () => {
+   const res = await fetch("https://api.adviceslip.com/advice", {cache: 'no-store'});
+   const data = await res.json();
+
+   
+   id.textContent = data.slip.id;
+   advice.textContent = `"${data.slip.advice}"`;
+}
+fetchAdvice();
+
+document.querySelector('.c-circle').addEventListener('click', fetchAdvice);
