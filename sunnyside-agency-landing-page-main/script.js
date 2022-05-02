d983d35c2aef57ed2846eddf5beffc0e45afd451
bits-wa <bitswa@pm.me>
2022-05-02 03:22:46 -0300

    fixes

---
+++ sunnyside-agency-landing-page-main/script.js
@@ -0,0 +1,12 @@
+let $headerBox = document.querySelector('.c-header__box')
+let $headerIcon = document.querySelector('.c-hamburguer')
+
+$headerIcon.addEventListener('click', e => {
+
+   if($headerBox.classList.contains('isVisible')) {
+      $headerBox.classList.remove('isVisible');
+      return
+   }
+
+   $headerBox.classList.add('isVisible');
+})
